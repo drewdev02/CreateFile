7b5d711d961291dc05cc5178915f75b76f9c9b91
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-03-10 17:19:39 -0500

    MOD

--- src/main/java/Entity/File.java
+++ src/main/java/Entity/File.java
@@ -1,6 +1,8 @@
 package Entity;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.thedeanda.lorem.LoremIpsum;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +12,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -29,13 +32,7 @@ public class File {
 
         }
 
-        public Element(long id,
-                       String name,
-                       String model,
-                       String description,
-                       String img_url,
-                       double price
-        ) {
+        public Element(long id, String name, String model, String description, String img_url, double price) {
             this.id = id;
             this.name = name;
             this.model = model;
@@ -47,32 +44,19 @@ public class File {
 
     }
 
-    public static @NotNull String toJson(@NotNull Element element) {
-        var gson = new Gson();
-        return "{\n" +
-                gson.toJson("id") + " : " + gson.toJson(element.id) + ",\n" +
-                gson.toJson("name") + " : " + gson.toJson(element.name) + ",\n" +
-                gson.toJson("model") + " : " + gson.toJson(element.model) + ",\n" +
-                gson.toJson("description") + " : " + gson.toJson(element.description) + ",\n" +
-                gson.toJson("img_url") + " : " + gson.toJson(element.img_url) + ",\n" +
-                gson.toJson("price") + " : " + gson.toJson(element.price) +
-                "\n}";
-    }
-
-    /*public static String toJsonString(@NotNull Object object) {
+    public static @NotNull String toJson(@NotNull Object object) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Field[] fields = object.getClass().getDeclaredFields();
         JsonObject jsonObject = new JsonObject();
-        for (Field field : fields) {
+        for (Field field : object.getClass().getDeclaredFields()) {
             try {
                 field.setAccessible(true);
-                jsonObject.addProperty(field.getName(), gson.toJson(field.get(object)));
-            } catch (IllegalAccessException e) {
-                // handle exception
+                jsonObject.add(field.getName(), gson.toJsonTree(field.get(object)));
+            } catch (IllegalAccessException err) {
+                System.out.println(err);
             }
         }
         return gson.toJson(jsonObject);
-    }*/
+    }
 
 
     public static @NotNull Element randowElement() {
@@ -96,10 +80,7 @@ public class File {
         }
     }
 
-    public static void CreateFile(
-            String path,
-            @NotNull String name,
-            @NotNull List<Element> elements
+    public static void CreateFile(String path, @NotNull String name, @NotNull List<Element> elements
 
     ) {
         try {
@@ -122,11 +103,10 @@ public class File {
     }
 
     public static void main(String[] args) {
-        var path = "../../";
         var name = "elements";
         var elements = new ArrayList();
-        createElement(elements, 50);
-        File.CreateFile(path, name, elements);
+        createElement(elements, 2);
+        File.CreateFile(name, elements);
 
 
     }
